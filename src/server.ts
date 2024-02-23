4fcb9981937ee758bca12970f135846338da09f7
Javi-Gallego <galgar@gmail.com>
2024-02-23 11:05:23 +0100

    feat: added dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 
 import express, { Application } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app:Application = express();
 
-app.listen(4000, () => {
-    console.log('Server is running');
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
