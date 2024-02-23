5578c94d3e9b25dfba3a1117439a04dc582e586b
Javi-Gallego <galgar@gmail.com>
2024-02-23 11:19:11 +0100

    feat: healthy endpoint

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,15 @@ const app:Application = express();
 
 const PORT = process.env.PORT || 4001;
 
+app.get("/healthy", (req, res) => {
+    res.json(
+        {
+            success: true,
+            message: "Server is healthy"
+        }
+    );
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
