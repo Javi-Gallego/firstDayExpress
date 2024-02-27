88be29d229d84774c9f95a6fe5d70301affb03c5
Javi-Gallego <galgar@gmail.com>
2024-02-27 10:49:44 +0100

    feat: model role created

--- src/database/db.ts
+++ src/database/db.ts
@@ -7,6 +7,7 @@ import { Authors1708949717657 } from "./migrations/1708949717657-authors"
 import { Books1708949730694 } from "./migrations/1708949730694-books"
 import { Loans1708949738502 } from "./migrations/1708949738502-loans"
 import { Favorite_books1708949747619 } from "./migrations/1708949747619-favorite_books"
+import { AddIsActiveColumnToUsers1709024951852 } from "./migrations/1709024951852-add_is_active_column_to_users"
 
 export const AppDataSource = new DataSource ({
     type: "mysql",
@@ -16,7 +17,14 @@ export const AppDataSource = new DataSource ({
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_DATABASE || "test",
     entities: [],
-    migrations: [Roles1708945160731,Users1708948565273,Authors1708949717657,Books1708949730694,Loans1708949738502,Favorite_books1708949747619],
+    migrations: [
+        Roles1708945160731,
+        Users1708948565273,
+        Authors1708949717657,
+        Books1708949730694,
+        Loans1708949738502,
+        Favorite_books1708949747619
+    ],
     synchronize: false,
     logging: false,
 })
